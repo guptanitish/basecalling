f3092fe5f7d39a8944461a9c037e0c83ad5afde4
Srikant Aggarwal <Srikant@Srikants-MacBook-Pro.local>
2014-12-14 22:37:33 -0500

    Blast related changes

--- blast/RemoveGap.java
+++ blast/RemoveGap.java
@@ -1,53 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-public class RemoveGap {
+public class RemoveGap{
 	public static void main(String[] args) throws IOException {
-		String path = args[0];
-		String fileName = path.substring(path.lastIndexOf('/')+1, path.lastIndexOf('.'));
-		String folder = path.substring(0, path.lastIndexOf('/'));
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		PrintWriter out = new PrintWriter(folder+"/"+fileName+".alignedfasta");
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		String folder = args[0];
+		File dir = new File(folder);
+
+                File[] outFiles = dir.listFiles(new FilenameFilter() {
+                        public boolean accept(File dir, String name) {
+                                return name.toLowerCase().endsWith(".out");
+                        }
+                });
+
+		int outFileCount = outFiles.length;
+		BufferedReader br = null;
+		PrintWriter out = null;
 		int start = -1, end = -1;
 		String query = null;
 		String subject = null;
-		
-		if (st.hasMoreTokens()) 
-			start = Integer.parseInt(st.nextToken());
-		if (st.hasMoreTokens())
-			end = Integer.parseInt(st.nextToken());
-		if (st.hasMoreTokens())
-			query = st.nextToken();
-		if (st.hasMoreTokens())
-			subject = st.nextToken();
+		StringTokenizer st = null;	
+	
+		for (int c = 0; c < outFileCount; ++c) { 
+			File outFile = outFiles[c];
+			String fileName = outFile.getName();
+			String fileNameWoExt = fileName.substring(0,fileName.lastIndexOf("."));
+			br = new BufferedReader(new FileReader(outFile));
+			out = new PrintWriter(folder+"/"+fileNameWoExt+".alignedfasta");
+			st = new StringTokenizer(br.readLine());
+			
+			if (st.hasMoreTokens()) 
+				start = Integer.parseInt(st.nextToken());
+			if (st.hasMoreTokens())
+				end = Integer.parseInt(st.nextToken());
+			if (st.hasMoreTokens())
+				query = st.nextToken();
+			if (st.hasMoreTokens())
+				subject = st.nextToken();
 
-		out.println(start);
-		out.println(end);
+			out.println(start);
+			out.println(end);
 
-		int length1 = query.length();
-		int length2 = subject.length();
+			int length1 = query.length();
+			int length2 = subject.length();
 
-		if (length1 == length2) {
-			StringBuffer temp1 = new StringBuffer();
-			StringBuffer temp2 = new StringBuffer();
-			for (int i = 0; i < length1; ++i) {
-				char ch1 = query.charAt(i);
-				char ch2 = subject.charAt(i);	
-				if (ch1 != '-') {
-					temp1.append(ch1);
-					temp2.append(ch2);
+			if (length1 == length2) {
+				StringBuffer temp1 = new StringBuffer();
+				StringBuffer temp2 = new StringBuffer();
+				for (int i = 0; i < length1; ++i) {
+					char ch1 = query.charAt(i);
+					char ch2 = subject.charAt(i);	
+					if (ch1 != '-') {
+						temp1.append(ch1);
+						temp2.append(ch2);
+					}
 				}
+
+				if ((end-start+1) != temp1.length())
+					System.out.println("Query string doesn't agree with start and end, start =  " + start + ", end = " + end + ", length = " + temp1.length());
+				out.println(temp1.toString());
+				out.println(temp2.toString()); 
 			}
 
-			if ((end-start+1) != temp1.length())
-				System.out.println("Query string doesn't agree with start and end, start =  " + start + ", end = " + end + ", length = " + temp1.length());
-			out.println(temp1.toString());
-			out.println(temp2.toString()); 
+			out.flush();
+			out.close();
 		}
-
-		out.flush();
-		out.close();
 
 		System.exit(0);
 	}
